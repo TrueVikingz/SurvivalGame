aa942bd5ca7fec8abe17e53026ac08dd2661688d
TimØ <89967395+TrueVikingz@users.noreply.github.com>
2022-01-11 15:04:59 +0100

    Main progress 1

---
+++ Survival game/Assets/Scripts/Collidable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    public ContactFilter2D filter;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10];
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+}
