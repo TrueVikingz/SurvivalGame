63a9660ee5ff6fada06308216bebff3245d39254
TimØ <89967395+TrueVikingz@users.noreply.github.com>
2022-01-20 10:02:16 +0100

    Floating text

---
+++ Survival game/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText
+{
+    public bool active;
+    public GameObject go;
+    public Text txt;
+    public Vector3 motion;
+    public float duration;
+    public float lastShown;
+
+    public void show()
+    {
+        active = true;
+        lastShown = Time.time;
+        go.SetActive(true);
+
+    }
+
+    public void Hide()
+    {
+        active = false;
+        go.SetActive(active);
+    }
+
+    public void UpdateFloatingText()
+    {
+        if (!active)
+            return;
+
+        // if   10      -     7    >      2
+        if (Time.time - lastShown > duration)
+            Hide();
+
+        go.transform.position += motion * Time.deltaTime;
+    }
+}
---
+++ Survival game/Assets/Scripts/FloatingTextManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextManager : MonoBehaviour
+{
+    public GameObject textContainer;
+    public GameObject textPrefab;
+
+    private List<FloatingText> floatingTexts = new List<FloatingText>();
+
+    private FloatingText GetFloatingText()
+    {
+        FloatingText txt = floatingTexts.Find(t => !t.active);
+
+        if (txt == null)
+        {
+            txt = new FloatingText();
+            txt.go = Instantiate(textPrefab);
+            txt.go.transform.SetParent(textContainer.transform);
+            txt.txt = txt.go.GetComponent<Text>();
+
+
+        }
+        return;
+    }
+}
