f13fa08fe4253edd117d1dc0297be14263b06a80
TimØ <89967395+TrueVikingz@users.noreply.github.com>
2022-01-20 10:31:20 +0100

    Update FloatingTextManager.cs

--- Survival game/Assets/Scripts/FloatingTextManager.cs
+++ Survival game/Assets/Scripts/FloatingTextManager.cs
@@ -9,6 +9,28 @@ public class FloatingTextManager : MonoBehaviour
 
     private List<FloatingText> floatingTexts = new List<FloatingText>();
 
+    private void Update()
+    {
+        foreach (FloatingText txt in floatingTexts)
+            txt.UpdateFloatingText();
+    }
+
+    public void Show(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        FloatingText floatingText = GetFloatingText();
+
+        floatingText.txt.text = msg;
+        floatingText.txt.fontSize = fontSize;
+        floatingText.txt.color = color;
+
+        floatingText.txt.transform.position = Camera.main.WorldToScreenPoint(position); // Transfer world space to screen space so we can use it in UI
+        floatingText.motion = motion;
+        floatingText.duration = duration;
+
+        floatingText.show();
+
+    }
+
     private FloatingText GetFloatingText()
     {
         FloatingText txt = floatingTexts.Find(t => !t.active);
@@ -20,8 +42,8 @@ public class FloatingTextManager : MonoBehaviour
             txt.go.transform.SetParent(textContainer.transform);
             txt.txt = txt.go.GetComponent<Text>();
 
-
+            floatingTexts.Add(txt);
         }
-        return;
+        return txt;
     }
 }
