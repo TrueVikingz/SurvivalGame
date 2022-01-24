984e6836f2c21ebc4d6f2df9273fb02330e5afbe
TimØ <89967395+TrueVikingz@users.noreply.github.com>
2022-01-24 16:12:54 +0100

    Swing animation

--- Survival game/Assets/Scripts/Weapon.cs
+++ Survival game/Assets/Scripts/Weapon.cs
@@ -13,6 +13,7 @@ public class Weapon : Collidable
     private SpriteRenderer spriteRenderer;
 
     // Swing
+    private Animator anim;
     private float cooldown = 0.5f;
     private float lastSwing;
 
@@ -20,6 +21,7 @@ public class Weapon : Collidable
     {
         base.Start();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     protected override void Update()
@@ -59,7 +61,8 @@ public class Weapon : Collidable
 
     private void Swing()
     {
-        Debug.Log("Swing");
+        anim.SetTrigger("Swing");
+
     }
 
 }
